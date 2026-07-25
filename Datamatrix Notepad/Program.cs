c5cc8d87f5641bf42af35d1d1ad7b00b94ea406b
muzhigg <88670581+muzhigg@users.noreply.github.com>
2026-07-25 14:22:10 +0300

    feat: add serial diagnostics page

--- Datamatrix Notepad/Program.cs
+++ Datamatrix Notepad/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.FluentUI.AspNetCore.Components;
 using Datamatrix_Notepad;
+using Datamatrix_Notepad.Services.Serial;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
+builder.Services.AddScoped<SerialPortService>();
 
 await builder.Build().RunAsync();
